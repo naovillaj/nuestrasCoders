3dcc9fdc4645b9dc418e2b99bf0e714da513ac33
Naomi Villanueva <naovillaj@gmail.com>
2017-04-19 00:15:18 -0500

    imagenes

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+
+var parrafo = document.createElement("h2");
+parrafo.innerText = "Nuestras Coders";
+document.body.appendChild(parrafo);
+
+var hr = document.createElement("hr");
+hr.style.size = "10px";
+hr.style.width = "5%;"
+document.body.appendChild(hr);
+
+var div = [];
+var imagen = [];
+var span = [];
+    for(var i=1; i <= 6; i++){
+
+        div[i] = document.createElement("div");
+        imagen[i] = document.createElement("img");
+        imagen[i].setAttribute("src", "assets/img/"+i+".png");
+
+        span[i] = document.createElement("span");
+        span[i].setAttribute("color", "white");
+
+        document.body.appendChild(div[i]);
+        div[i].appendChild(imagen[i]);
+        div[i].appendChild(span[i]);
+    }
+
+span[1].innerText = "Flor de Cantuta Tello";
+span[2].innerText = "Naomi Villanueva";
+span[3].innerText = "Miriam Peralta";
+span[4].innerText = "Karin Alejo";
+span[5].innerText = "Liliana Peña";
+span[6].innerText = "Ruth Salvador";
